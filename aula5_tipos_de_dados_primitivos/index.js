d7e96a432c0a2f050fb355cfe347e946c5ae7a04
reenanrs <renan.victor325@outlook.com>
2024-01-24 12:26:16 -0300

    atualizacoes

---
+++ aula5_tipos_de_dados_primitivos/index.js
@@ -0,0 +1,16 @@
+//String, number, undefined, null, boolean, symbol
+
+const nome = 'Luiz'; //Pode ser com aspas simples/aspas duplas, pode-se utilizar também com crazi
+const num1 = 10; //number
+const num2 = 10.52 //number
+let nomeAluno; // undefinded = não aponta pra local nenhuma na memória
+let  sobrenomeAluno = null; //Nulo -: não aponta pra local nenhuma na memória
+const aprovado = false; //Dados boolean = true, false (lógico)
+const reprovado = true; //Dados boolean = true, false (lógico)
+
+console.log( typeof nome, nome);
+
+const a = [1, 2];
+const b = a; //sinal de = seria atribuição, b atribui a, tornando iguais.
+
+console.log(a, b);
---
+++ aula6_operadores_aritmeticos/index.js
@@ -0,0 +1,31 @@
+/**
+ *Aritméticos
+ *+ Adição / Concatenação ( - / *)
+ * **potenciação
+ * % Resto da divisão
+ * precdencia : (), **, *, /, %, +, -
+ *  Incremento = ++
+ *  Decremento = --
+ *  NaN - Not a number... ParseInt(inteiro), parseFloat(Decimais), number(converte Inteiro, decimais)
+ */
+
+ const num1 = 5;
+ const num2 = 10;
+ console.log(num1 + num2)
+
+ let contador = 1;
+ contador++; // 2
+ contador++; // 3
+ contador++; // 4
+ contador++; // 5
+console.log('contador: ',contador);
+
+const passo = 5;
+let contador1 = 0;
+
+contador1 = contador1 + passo;
+console.log(contador1);
+contador1 = contador1 + passo;
+console.log(contador1);
+contador1 = contador1 + passo;
+console.log(contador1);
